8d82428b2518ba254734538bd95b394fc3c3c792
Christopher Romero <romero.chrisprof@gmail.com>
2024-08-07 00:47:24 -0500

    Updated database, added values

---
+++ db/seeds.sql
@@ -0,0 +1,46 @@
+
+-- Insert pre data into department
+INSERT INTO department (name) VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title, salary, department) VALUES
+('Sales Lead',        100000,  1),
+('Salesperson',        80000,  1),
+('Lead Engineer',     150000,  2),
+('Software Engineer', 120000,  2),
+('Account Manager',   160000,  3),
+('Accountant',        125000,  3),
+('Legal Team Lead',   250000,  4),
+('Lawyer',            190000,  4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('John',    'Doe', 1, NULL),      -- Sales Lead
+('Mike',     'Chan', 2, 1),       -- Salesperson, manager is John
+('Ashley', 'Rodriguez', 3, NULL), -- Lead Engineer
+('Kevin',   'Tupic', 4, 3),       -- Software Engineer, manager is Ashley
+('Kunal',     'Singh', 5, NULL),  -- Account Manager
+('Malia',   'Brown', 6, 5),       -- Accountant, manager is Kunal
+('Sarah',   'Lourd', 7, NULL),    -- Legal Team lead
+('Tom',    'Allen', 8, 7);        -- Lawyer, manager is Sarah
+
+
+-- This will be a table to display all employees
+-- SELECT
+--     employee.id AS employee_id,
+--     employee.first_name,
+--     employee.last_name,
+--     role.title AS role_title,
+--     role.salary AS role_salary,
+--     department.name AS department_name,
+--     CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+-- FROM
+--     employee
+-- JOIN
+--     role ON employee.role_id = role.id
+-- JOIN
+--     department ON role.department = department.id
+-- LEFT JOIN
+--     employee AS manager ON employee.manager_id = manager.id;
